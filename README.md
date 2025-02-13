5b6d6354a40d3308c6137b6bd815422375080c56
rbtzuniga <rz2516@columbia.edu>
2025-02-13 16:15:27 -0500

    added flask api examples

--- README.md
+++ README.md
@@ -3,4 +3,9 @@
 This repository contains some examples related to the course labs. 
 
 ## Lab 02
-Find example [here](lab_02) and 
+Find example [here](lab_02) or open the notebook in Colab pressing  <a target="_blank" href="https://colab.research.google.com/github/advanced-computing/lab_examples/blob/main/lab_02/lab_02_example.ipynb">
+  <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
+</a>
+
+## Lab 04
+
---
+++ lab_04/flask_example_1.py
@@ -0,0 +1,39 @@
+from flask import Flask, jsonify, request
+import pandas as pd 
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    """Return a friendly HTTP greeting."""
+
+    return "<p>Hello, World!</p>"
+
+@app.route("/sum", methods=["GET"])
+def sum():
+    """Return the sum of two numbers."""
+
+    a = request.args.get("a")
+    b = request.args.get("b")
+
+    return jsonify({"sum": int(a) + int(b)})
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+@app.route("/factorial", methods=["GET"])
+def factorial_route():
+    """Return the factorial of a number."""
+
+    n = request.args.get("n",10)
+    r = factorial(int(n))
+    response = f'{n}! equals {r}'
+    return response
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
