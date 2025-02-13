b808eced5cf54367a7ca67ea40fe6e42b89d8d74
rbtzuniga <rz2516@columbia.edu>
2025-02-13 16:23:35 -0500

    added lab04 example solution

---
+++ lab_04/flask_lab_example.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, request
+import pandas as pd 
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    """Return a friendly HTTP greeting."""
+
+    return "<p>Hello, World!</p>"
+
+
+@app.get("/api/list")
+def list_2():
+    format = request.args.get('format', 'json')
+    filterby = request.args.get('filterby',None)
+    filtervalue = request.args.get('filtervalue',None)
+    limit = int(request.args.get('limit', 1000))
+    offset = int(request.args.get('offset', 0))
+
+    data = pd.read_csv('videos_data.csv')
+    if filterby:
+        if filtervalue is None:
+            return "Invalid filtervalue"
+        elif filterby not in data.columns:
+            return "Invalid filterby column"
+        else:
+            data = data[data[filterby] == int(filtervalue)]
+    data = data.iloc[offset:offset+limit]
+    if format == 'json':
+        return jsonify(data.to_dict(orient='records'))
+    elif format == 'csv':
+        return data.to_csv(index=False)
+    else:
+        return "Invalid format"
+
+if __name__ == "__main__":
+    app.run(debug=True)
